deda44fc48019875dea126e18b35191c433ec7e3
ken <ken556621@gmail.com>
2020-01-23 02:34:52 +0800

    Add login js not compelete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Login from './components/login'
+
+
+class App extends Component {
+    render() { 
+        return (
+            <div>
+                <Login />
+            </div>
+        );
+    }
+}
+ 
+ReactDOM.render(<App />, document.getElementById('root'));
