40f17bf5b5e6c89754d9062352acd7321d261b02
ken <ken556621@gmail.com>
2020-02-06 08:04:21 +0800

    Fetch google map place api

--- src/components/findPlace.js
+++ src/components/findPlace.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import firebase from './common/firebase';
 import axios from 'axios';
 
 import Ball from './common/ballImg';
@@ -16,6 +17,7 @@ class FindPlace extends Component {
         this.state = {
             userLat: 25.0424536,
             userLng: 121.562731,
+            isLoading: false,
             court: []
         }
     }
@@ -38,9 +40,45 @@ class FindPlace extends Component {
     }
 
     getCoordinates = (position) => {
+        const db = firebase.firestore();
         this.setState({
             userLat: position.coords.latitude,
-            userLng: position.coords.longitude
+            userLng: position.coords.longitude,
+            isLoading: true
+        })
+        axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=籃球場&location=${position.coords.latitude},${position.coords.longitude}&radius=10000&key=AIzaSyAOCD6zBK2oD6Lrz3gN5zNxM-GNDatpE-o`)
+        .then(data => {
+            data.results.forEach(place => {
+                    //存入 DB
+                db.collection("locations").doc(place.id).set({
+                    id: place.id,
+                    name: place.name,
+                    address: place.formatted_address,
+                    lat: place.geometry.location.lat,
+                    lng: place.geometry.location.lng,
+                    global_code: place.plus_code.global_code
+                })
+                .then(function() {
+                    console.log("Document successfully written!");
+                    //改變狀態
+                    this.setState({
+                        court: {
+                            id: place.id,
+                            name: place.name,
+                            address: place.formatted_address,
+                            lat: place.geometry.location.lat,
+                            lng: place.geometry.location.lng,
+                            global_code: place.plus_code.global_code
+                        }
+                    })
+                })
+                .catch(function(error) {
+                    console.error("Error writing document: ", error);
+                });
+            })
+            this.setState({
+                isLoading: false
+            })
         })
     }
 
@@ -74,11 +112,10 @@ class FindPlace extends Component {
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
                     >
-                    {
-                        <Ball
-                            lat={ this.state.userLat }
-                            lng={ this.state.userLng }
-                        />
+                    {   
+                        this.state.court.map(place => {
+                            return <Ball lat={ place.lat } lng={ place.lng } />
+                        })
                     }
                     </GoogleMapReact>
                 </div>
--- src/components/placeInfo.js
+++ src/components/placeInfo.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Navbar from './common/navbar';
-import Card from './common/card';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SportsBasketballIcon from '@material-ui/icons/SportsBasketball';
 
+
+import '../styles/placeInfo.scss';
 
 class PlaceInfo extends Component {
     constructor(props){
@@ -11,9 +19,33 @@ class PlaceInfo extends Component {
     }
     render() { 
         return ( 
-            <div>
+            <div className="place-info-container">
                 <Navbar history={ this.props.history } />
-                <Card />
+                <Card className="card">
+                    <CardHeader
+                        className="card-header"
+                        avatar={
+                        <Avatar className="weather-img" aria-label="recipe">
+                            R
+                        </Avatar>
+                        }
+                        action={
+                        <IconButton className="setting-btn" aria-label="settings">
+                            <SportsBasketballIcon />
+                        </IconButton>
+                        }
+                        title="台北和平籃球館"
+                        subheader="106台灣台北市大安區敦南街76巷28號"
+                    />
+                    <div className="image">
+                        <img src="https://images.unsplash.com/photo-1537835441678-0d136a68fe01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"/>
+                    </div>
+                    <CardContent className="card-content">
+                        <Typography className="card-words" variant="body2" color="textSecondary" component="p">
+                        
+                        </Typography>
+                    </CardContent>
+                </Card>
             </div>
         );
     }
